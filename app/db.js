1aac33acadb2ab686548fe967086e19ee5bf5e24
miltoncosme <milton@sriautomacao.com.br>
2021-03-15 19:53:51 -0300

    atualizacao do pg

--- app/db.js
+++ app/db.js
@@ -1,10 +1,16 @@
 const conn = function(){
     return {
-    user: process.env.USER_DB,
-    password: process.env.PASS_DB,
-    host: 'localhost',
-    database: process.env.NAME_DB,
-    port: 5432}
+        user: process.env.USER_DB,
+        password: process.env.PASS_DB,
+        host: 'localhost',
+        database: process.env.NAME_DB,
+        port: 5432,
+        ssl: false,
+        max: 25, 
+        idleTimeoutMillis: 3000, 
+        connectionTimeoutMillis: 3000, 
+        maxUses: 7500,
+    }
 }
 
 module.exports.conn = conn
